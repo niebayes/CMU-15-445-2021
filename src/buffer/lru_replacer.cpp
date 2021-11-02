2d55a2ab450aa6a97bad3e42b27bb3246e1ceb92
niebayes <niebayes@gmail.com>
2021-11-02 10:59:13 +0800

    add LRU replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -10,26 +10,53 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <cassert>
+
 #include "buffer/lru_replacer.h"
 
 namespace bustub {
 
-// General Hints:
-// → Your LRUReplacer needs to check the "pinned" status of
-// a Page.
-// → If there are no pages touched since last sweep, then
-// return the lowest page id.
-
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+  // reserve space for hash map.
+  ump_.reserve(num_pages);
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return true; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  // if no frame existed.
+  if (ump_.empty()) {
+    return false;
+  }
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+  // evict the least recently used frame.
+  *frame_id = lst_.back();
+  lst_.pop_back();
+  ump_.erase(*frame_id);
+  assert(lst_.size() == ump_.size());
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+  return true;
+}
 
-size_t LRUReplacer::Size() { return 0; }
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  // if the frame does not exist.
+  if (ump_.count(frame_id) == 0) {
+    return;
+  }
+
+  // remove the frame both from the hash map and the list.
+  lst_.erase(ump_.at(frame_id));
+  ump_.erase(frame_id);
+  assert(lst_.size() == ump_.size());
+}
+
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  // add the newly unpinned both to the hash map and the list.
+  lst_.push_front(frame_id);
+  ump_[frame_id] = lst_.begin();
+  assert(lst_.size() == ump_.size());
+}
+
+size_t LRUReplacer::Size() { return ump_.size(); }
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -15,6 +15,7 @@
 #include <list>
 #include <mutex>  // NOLINT
 #include <vector>
+#include <unordered_map>
 
 #include "buffer/replacer.h"
 #include "common/config.h"
@@ -47,6 +48,12 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+
+  // newly unpinned frames are inserted to the front.
+  // least recently used frames are fetched from the back.
+  std::list<frame_id_t> lst_;
+  // <frame_id, frame_pos_in_lst>
+  std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> ump_;
 };
 
 }  // namespace bustub
